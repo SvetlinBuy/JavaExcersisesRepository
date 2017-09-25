68dfbf1d6f0662ce356d2472b8dd5e0c9f2052dd
Svetlin Buy <svetlinbuy@gmail.com>
2017-09-25 21:20:43 +0300

    Pattern A ready

--- Java Fundamentals/Java Advanced/Excersises/Abstraction/01_FilltheMatrix/src/FilltheMatrix.java
+++ Java Fundamentals/Java Advanced/Excersises/Abstraction/01_FilltheMatrix/src/FilltheMatrix.java
@@ -22,18 +22,37 @@ public class FilltheMatrix {
     }
 
     private static void printMatrix(int[][] matrix) {
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                System.out.print(matrix[i][j]+" ");
+
+            }
+
+            System.out.println();
+        }
+
     }
 
     private static void FillMatrixPatternB(int[][] matrix, int matrixSize) {
-        
-        
-        
+
+
     }
 
     private static void FillMatrixPatternA(int[][] matrix, int matrixSize) {
-        
-        
-        
+
+
+        int startNumb = 1;
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                matrix[j][i] += startNumb;
+                startNumb++;
+            }
+        }
     }
 
 }
