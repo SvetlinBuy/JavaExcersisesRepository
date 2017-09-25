71eae34db227067df6f4403100f6e37a3561f66e
Svetlin Buy <svetlinbuy@gmail.com>
2017-09-25 22:08:39 +0300

    update Matrix of Palindromes done 100/100 in Judge

--- Java Fundamentals/Java Advanced/Excersises/Abstraction/2_MatrixofPalindromes/src/MatrixofPalindromes.java
+++ Java Fundamentals/Java Advanced/Excersises/Abstraction/2_MatrixofPalindromes/src/MatrixofPalindromes.java
@@ -8,11 +8,11 @@ public class MatrixofPalindromes {
         String[] initalInput = input.nextLine().split(" ");
 
         int rowCount = Integer.parseInt(initalInput[0]);
-        int columnCount = Integer.parseInt(initalInput[0]);
+        int columnCount = Integer.parseInt(initalInput[1]);
 
         String[][] matrix = new String[rowCount][columnCount];
 
-        PalidromeMatrixFiller(matrix, rowCount, columnCount);
+        PalidromeMatrixFiller(matrix);
 
 
         printMatrix(matrix);
@@ -20,16 +20,23 @@ public class MatrixofPalindromes {
 
     }
 
-    private static void PalidromeMatrixFiller(String[][] matrix, int rowCount, int columnCount) {
+    private static void PalidromeMatrixFiller(String[][] matrix) {
 
+        Character row = 'a';
 
         for (int i = 0; i < matrix.length ; i++) {
 
-            Character row = 'a';
+
+            Character column = row;
+
 
             for (int j = 0; j < matrix[0].length; j++) {
 
+                 String element = row + "" + column + "" + row;
 
+                 matrix[i][j] = element;
+
+                 column++;
 
             }
 
