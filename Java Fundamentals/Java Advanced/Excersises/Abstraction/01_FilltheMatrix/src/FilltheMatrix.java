714a8270775c8a4f9bd4dd6a1cf7794e52c17694
Svetlin Buy <svetlinbuy@gmail.com>
2017-09-25 20:57:35 +0300

    adding the body for hte Fill the Matrix

--- Java Fundamentals/Java Advanced/Excersises/Abstraction/01_FilltheMatrix/src/FilltheMatrix.java
+++ Java Fundamentals/Java Advanced/Excersises/Abstraction/01_FilltheMatrix/src/FilltheMatrix.java
@@ -1,7 +1,37 @@
+import java.util.Scanner;
+
 public class FilltheMatrix {
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        int matrixSize = Integer.parseInt(input.nextLine());
+        String matrixPattern = input.nextLine();
+        int[][] matrix = new int[matrixSize][matrixSize];
+
+        if (matrixPattern.equals("A")) {
+            FillMatrixPatternA(matrix, matrixSize);
+        } else if (matrixPattern.equals("B")) {
+            FillMatrixPatternB(matrix, matrixSize);
+        }
+
+        printMatrix(matrix);
+    }
+
+    private static void printMatrix(int[][] matrix) {
+    }
+
+    private static void FillMatrixPatternB(int[][] matrix, int matrixSize) {
+        
+        
+        
+    }
+
+    private static void FillMatrixPatternA(int[][] matrix, int matrixSize) {
+        
+        
+        
     }
 
 }
