42d735715ce9ac8d1dfe2e9dccb9ba144e556a6c
SIVIV <stoyan.ivanoff87@gmail.com>
2017-09-28 18:33:24 +0300

    01. CountSubStringOccurances 100/100

--- Java Fundamentals/Java Advanced/Excersises/StringProcessing/01_CountSubstringOccurrences/src/CountSubstringOccurrences.java
+++ Java Fundamentals/Java Advanced/Excersises/StringProcessing/01_CountSubstringOccurrences/src/CountSubstringOccurrences.java
@@ -4,11 +4,26 @@ public class CountSubstringOccurrences {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        public static void main(String[] args) {
 
+            Scanner scan = new Scanner(System.in);
+            String text = scan.nextLine().toLowerCase();
+            String pattern = scan.nextLine().toLowerCase();
 
-        //System.out.println("Stoyan Test");
+            printResult(findOccurancies(text,pattern));
+        }
 
+    private static int findOccurancies(String text, String pattern) {
+        int count = 0;
+        int i = 0;
+        while (text.indexOf(pattern, i) != -1) {
+            count++;
+            i = text.indexOf(pattern,i)+1;
+        }
+        return count;
+    }
 
+    private static void printResult(int count) {
+        System.out.println(count);
     }
 }
