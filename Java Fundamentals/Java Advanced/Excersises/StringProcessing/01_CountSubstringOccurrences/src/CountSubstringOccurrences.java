000568c588b4a1edf4497ae79fe9eac1ae92dad5
Svetlin Buy <svetlinbuy@gmail.com>
2017-09-28 17:43:46 +0300

    adding 1 exam 0/100 test

--- Java Fundamentals/Java Advanced/Excersises/StringProcessing/01_CountSubstringOccurrences/src/CountSubstringOccurrences.java
+++ Java Fundamentals/Java Advanced/Excersises/StringProcessing/01_CountSubstringOccurrences/src/CountSubstringOccurrences.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 public class CountSubstringOccurrences {
 
     public static void main(String[] args) {
 
-
+        Scanner input = new Scanner(System.in);
 
 
     }
