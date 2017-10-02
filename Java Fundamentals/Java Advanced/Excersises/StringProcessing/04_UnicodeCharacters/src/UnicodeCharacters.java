4c93a8f925f9cefb28801151ba113d0c0ef9d1b0
Svetlin Buy <svetlinbuy@gmail.com>
2017-10-02 03:01:27 +0300

    06 exam 60/100 05 exam 100/100 04 exam 100/100

--- Java Fundamentals/Java Advanced/Excersises/StringProcessing/04_UnicodeCharacters/src/UnicodeCharacters.java
+++ Java Fundamentals/Java Advanced/Excersises/StringProcessing/04_UnicodeCharacters/src/UnicodeCharacters.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class UnicodeCharacters {
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        String text = input.nextLine();
+        String output = EncodeCharactersToUnicode(text);
+        System.out.println(output);
+
+    }
+
+    private static String EncodeCharactersToUnicode(String text) {
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : text.toCharArray())
+        {
+            String encodedValue =String.format("\\u00%x",(int)c);
+            sb.append(encodedValue);
+        }
+        return sb.toString();
     }
 }
--- Java Fundamentals/Java Advanced/Excersises/StringProcessing/06_MagicExchangeableWords/src/MagicExchangeableWords.java
+++ Java Fundamentals/Java Advanced/Excersises/StringProcessing/06_MagicExchangeableWords/src/MagicExchangeableWords.java
@@ -1,6 +1,42 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class MagicExchangeableWords {
 
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String[] inputArray = input.nextLine().split(" ");
+
+        isExchangable(inputArray);
+    }
+
+    private static void isExchangable(String[] inputArray) {
+
+        char[] firstWord = inputArray[0].toCharArray();
+        char[] secondWord = inputArray[1].toCharArray();
+
+        Map<Character, Character> exchangable = new LinkedHashMap<>();
+        boolean result = true;
+
+        if (firstWord.length == secondWord.length) {
+            for (int i = 0; i < firstWord.length; i++) {
+                if (exchangable.containsKey(firstWord[i])) {
+                    if (exchangable.get(firstWord[i]) != secondWord[i]) {
+                        result = false;
+                        break;
+                    }
+                } else {
+                    exchangable.put(firstWord[i], secondWord[i]);
+                }
+            }
+        } else {
+            result = false;
+        }
+
+        System.out.println(result);
 
     }
 }
