b4d4654902f7c61309db3f1a5327945fd767a9d8
Svetlin Buy <svetlinbuy@gmail.com>
2017-10-03 14:53:00 +0300

    14 exam 80/100

--- Java Fundamentals/Java Advanced/Excersises/StringProcessing/14_SumofAllValues/src/SumofAllValues.java
+++ Java Fundamentals/Java Advanced/Excersises/StringProcessing/14_SumofAllValues/src/SumofAllValues.java
@@ -1,6 +1,56 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SumofAllValues {
 
     public static void main(String[] args) {
 
+
+        Scanner scanner = new Scanner(System.in);
+
+        String keyString = scanner.nextLine();
+        String textString = scanner.nextLine();
+
+        Pattern patternStart = Pattern.compile("^[a-zA-Z_]+(?=\\d)");
+        Pattern patternEnd = Pattern.compile("(?<=\\d)[a-zA-Z_]+$");
+
+        Matcher matcherStart = patternStart.matcher(keyString);
+        Matcher matcherEnd = patternEnd.matcher(keyString);
+
+        String startKey;
+        String endKey;
+
+        if (matcherStart.find() && matcherEnd.find()) {
+            startKey = matcherStart.group();
+            endKey = matcherEnd.group();
+
+            String regex = startKey + "[\\d.]+?" + endKey;
+            Pattern digits = Pattern.compile(regex);
+            Matcher matchDigits = digits.matcher(textString);
+            double sum = 0;
+
+            while (matchDigits.find()) {
+
+                double num = Double.parseDouble(matchDigits.group()
+                        .substring(startKey.length(), matchDigits.group().length() - endKey.length()));
+                sum += num;
+            }
+
+            if (sum > 0) {
+
+                if (sum % 10 == 0) {
+                    System.out.printf("<p>The total value is: <em>%f</em></p>", sum);
+                }else {
+                    System.out.printf("<p>The total value is: <em>%.2f</em></p>", sum);
+                }
+
+            } else {
+                System.out.println("<p>The total value is: <em>nothing</em></p>");
+            }
+        }
+        else{
+            System.out.println("<p>A key is missing</p>");
+        }
     }
 }
