bc835848c0d9569c6fe88a3e3d634009b7a4dde3
Svetlin Buy <svetlinbuy@gmail.com>
2017-10-08 17:03:03 +0300

    updating files

--- Java Fundamentals/Java Advanced/Excersises/DataRepresentationandManipulation/07_FindtheMissingNumber/src/FindtheMissingNumber.java
+++ Java Fundamentals/Java Advanced/Excersises/DataRepresentationandManipulation/07_FindtheMissingNumber/src/FindtheMissingNumber.java
@@ -1,6 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class FindtheMissingNumber {
 
     public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numbersCount = Integer.parseInt(scanner.nextLine());
+
+   int[] arr = Arrays.stream(scanner.nextLine().split(", ")).mapToInt(m -> Integer.parseInt(m)).toArray();
+
+        Arrays.sort(arr);
+
+        int missing = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if(arr[0] != 1){
+
+                missing = 2;
+                break;
+            }
+
+            int num1 = arr[i];
+            int num2 =arr[i-1] + 1;
+
+
+            if(num1 != num2) {
+
+                missing = num1;
+                break;
+            }
+        }
+
+        System.out.println(missing -1);
 
     }
 }
