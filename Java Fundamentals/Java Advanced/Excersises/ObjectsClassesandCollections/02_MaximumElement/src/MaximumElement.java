86c4af2c8b95d910f4dd0c241b4ee762b5c77a6d
Svetlin Buy <svetlinbuy@gmail.com>
2017-10-08 23:40:39 +0300

    02 exam 100/100 03 exam 100/100

--- Java Fundamentals/Java Advanced/Excersises/ObjectsClassesandCollections/02_MaximumElement/src/MaximumElement.java
+++ Java Fundamentals/Java Advanced/Excersises/ObjectsClassesandCollections/02_MaximumElement/src/MaximumElement.java
@@ -1,6 +1,41 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class MaximumElement {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int commandsCount = Integer.parseInt(scanner.nextLine());
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+
+
+        for (int i = 0; i < commandsCount; i++) {
+
+            int[] command = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(n -> Integer.parseInt(n)).toArray();
+
+            if (command[0] == 1) {
+
+                stack.push(command[1]);
+
+            } else if (command[0] == 2) {
+
+                stack.pop();
+
+
+            } else if (command[0] == 3) {
+
+                List<Integer> listNumbers = stack.stream().collect(Collectors.toList());
+
+                Collections.sort(listNumbers);
+
+                System.out.println(listNumbers.get(listNumbers.size() -1));
+            }
+
+        }
 
     }
 }
--- Java Fundamentals/Java Advanced/Excersises/ObjectsClassesandCollections/03_BasicQueueOperations/src/BasicQueueOperations.java
+++ Java Fundamentals/Java Advanced/Excersises/ObjectsClassesandCollections/03_BasicQueueOperations/src/BasicQueueOperations.java
@@ -1,6 +1,53 @@
+import com.sun.org.apache.xerces.internal.impl.xs.SchemaNamespaceSupport;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class BasicQueueOperations {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+       int[] commands = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(m -> Integer.parseInt(m)).toArray();
+
+        Deque<Integer> queue = new ArrayDeque<>();
+
+
+        for (int i = 0; i < commands[0]; i++) {
+
+            int number = scanner.nextInt();
+
+            queue.add(number);
+
+        }
+
+        for (int i = 0; i < commands[1]; i++) {
+
+            queue.poll();
+
+        }
+
+        if (queue.contains(commands[2])) {
+
+            System.out.println(true);
+
+        } else {
+
+            if (queue.peek() == null) {
+                System.out.println(0);
+            } else {
+
+                List<Integer> numbers = queue.stream().collect(Collectors.toList());
+
+                Collections.sort(numbers);
+
+
+                System.out.println(numbers.get(0));
+            }
+        }
+
+
 
     }
 }
