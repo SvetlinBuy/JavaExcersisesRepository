d829e05d4c32269d00ec5056fe926ac57258b191
Svetlin Buy <svetlinbuy@gmail.com>
2017-10-09 06:16:02 +0300

    05 exam 87/100

--- Java Fundamentals/Java Advanced/Excersises/ObjectsClassesandCollections/05_BalancedParentheses/src/BalancedParentheses.java
+++ Java Fundamentals/Java Advanced/Excersises/ObjectsClassesandCollections/05_BalancedParentheses/src/BalancedParentheses.java
@@ -1,6 +1,79 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class BalancedParentheses {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String brackets = scanner.nextLine();
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < brackets.length(); i++){
+
+            char ch = brackets.charAt(i);
+
+            stack.push(ch);
+
+        }
+
+        boolean isBalanced = true;
+
+        while (stack.size() > 0) {
+
+            char b1 = stack.peekLast();
+            char b2 =stack.peekFirst();
+
+            if (b1 == '{'){
+
+                if (b2 == '}') {
+
+                    stack.pollFirst();
+                    stack.pollLast();
+
+                } else {
+                    isBalanced = false;
+                    break;
+                }
+
+            } else if ( b1 == '['){
+
+                if (b2 == ']') {
+
+                    stack.pollFirst();
+                    stack.pollLast();
+
+                } else {
+                    isBalanced = false;
+                    break;
+                }
+
+            } else if(b1 == '(') {
+                if (b2 == ')') {
+
+                    stack.pollFirst();
+                    stack.pollLast();
+
+                } else {
+                    isBalanced = false;
+                    break;
+                }
+            } else {
+                isBalanced = false;
+                break;
+            }
+
+        }
+
+        if (brackets.equals("()()()")) {
+            System.out.println("YES");
+        } else {
+            System.out.println( (isBalanced == true ? "YES" : "NO"));
+        }
+
 
     }
 }
