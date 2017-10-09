371f86cdad72866097899ff56544b642621581b1
Svetlin Buy <svetlinbuy@gmail.com>
2017-10-09 11:47:07 +0300

    08 exam 100/100

--- Java Fundamentals/Java Advanced/Excersises/ObjectsClassesandCollections/08_EvaluateExpression/src/EvaluateExpression.java
+++ Java Fundamentals/Java Advanced/Excersises/ObjectsClassesandCollections/08_EvaluateExpression/src/EvaluateExpression.java
@@ -1,5 +1,117 @@
+import java.util.*;
+
+
 public class EvaluateExpression {
     public static void main(String[] args) {
 
+
+        Scanner scanner = new Scanner(System.in);
+
+        String infix = scanner.nextLine();
+
+        Double value = evaluate(toPostfix(infix));
+
+        System.out.printf("%.2f", value);
+
+
+    }
+    public static Double evaluate(String postfix) {
+        // Use a stack to track all the numbers and temporary results
+        Stack<Double> s = new Stack<Double>();
+
+        // Convert expression to char array
+        char[] chars = postfix.toCharArray();
+
+        // Cache the length of expression
+        int N = chars.length;
+
+        for (int i = 0; i < N; i++) {
+            char ch = chars[i];
+
+            if (isOperator(ch)) {
+                // Operator, simply pop out two numbers from stack and perfom operation
+                // Notice the order of operands
+                switch (ch) {
+                    case '+': s.push(s.pop() + s.pop());     break;
+                    case '*': s.push(s.pop() * s.pop());     break;
+                    case '-': s.push(-s.pop() + s.pop());    break;
+                    case '/': s.push(1 / s.pop() * s.pop()); break;
+                }
+            } else if(Character.isDigit(ch)) {
+                // Number, push to the stack
+                s.push(0.0);
+                while (Character.isDigit(chars[i]))
+                    s.push(10.0 * s.pop() + (chars[i++] - '0'));
+            }
+        }
+
+        // The final result should be located in the bottom of stack
+        // Otherwise return 0.0
+        if (!s.isEmpty())
+            return s.pop();
+        else
+            return 0.0;
+    }
+
+    /**
+     * Check if the character is an operator
+     */
+    private static boolean isOperator(char ch) {
+        return ch == '*' || ch == '/' || ch == '+' || ch == '-';
+    }
+
+    private static String toPostfix(String exp) {
+
+        String[] input = exp.split(" ");
+        ArrayDeque<String> operators = new ArrayDeque<>();
+        ArrayDeque<String> expression = new ArrayDeque<>();
+        Map<String, Integer> priorites = new HashMap<>();
+        priorites.put("*", 3);
+        priorites.put("/", 3);
+        priorites.put("+", 2);
+        priorites.put("-", 2);
+        priorites.put("(", 1);
+
+        String toReturn = "";
+
+        for (String anInput : input) {
+            try {
+                double num = Double.parseDouble(anInput);
+                expression.addLast(anInput);
+            } catch (Exception e) {
+                switch (anInput) {
+                    case "x":
+                        expression.addLast(anInput);
+                        break;
+                    case "(":
+                        operators.push(anInput);
+                        break;
+                    case ")":
+                        String symbol = operators.pop();
+                        while (!symbol.equals("(")) {
+                            expression.addLast(symbol);
+                            symbol = operators.pop();
+                        }
+                        break;
+                    default:
+                        while (!operators.isEmpty() && priorites.get(operators.peek()) >= priorites.get(anInput)) {
+                            expression.addLast(operators.pop());
+                        }
+                        operators.push(anInput);
+                        break;
+                }
+            }
+        }
+        while (!operators.isEmpty()) {
+            expression.addLast(operators.pop());
+        }
+
+        while (expression.size() > 0) {
+
+            toReturn += expression.pop() + " ";
+
+        }
+
+        return toReturn;
     }
 }
