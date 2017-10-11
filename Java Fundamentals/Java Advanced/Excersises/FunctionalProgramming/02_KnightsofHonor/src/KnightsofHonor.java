8dca01cf7de63c8ac3c81df4a2c7f90e0bd26d4e
Svetlin Buy <svetlinbuy@gmail.com>
2017-10-11 18:56:28 +0300

    01 02 03 100/100

--- Java Fundamentals/Java Advanced/Excersises/FunctionalProgramming/02_KnightsofHonor/src/KnightsofHonor.java
+++ Java Fundamentals/Java Advanced/Excersises/FunctionalProgramming/02_KnightsofHonor/src/KnightsofHonor.java
@@ -1,2 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class KnightsofHonor {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Arrays.stream(scanner.nextLine().split(" ")).forEach(st->{System.out.println("Sir " + st);});
+
+    }
 }
--- Java Fundamentals/Java Advanced/Excersises/FunctionalProgramming/03_CustomMinFunction/src/CustomMinFunction.java
+++ Java Fundamentals/Java Advanced/Excersises/FunctionalProgramming/03_CustomMinFunction/src/CustomMinFunction.java
@@ -1,2 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 public class CustomMinFunction {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] intArr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(m -> Integer.parseInt(m)).toArray();
+
+        Function<int[], Integer> findSmallest = a -> {
+
+            int n = Integer.MAX_VALUE;
+
+            for (int i = 0; i < a.length; i++) {
+
+                if (a[i] < n) {
+
+                    n = a[i];
+                }
+            }
+
+            return n;
+        };
+
+
+        System.out.println(findSmallest.apply(intArr));
+
+    }
 }
