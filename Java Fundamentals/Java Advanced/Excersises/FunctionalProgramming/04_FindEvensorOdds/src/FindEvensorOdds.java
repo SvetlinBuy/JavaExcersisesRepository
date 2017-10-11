f1ddf32c0c47a0609bc38085b23b95030c97a5f5
Svetlin Buy <svetlinbuy@gmail.com>
2017-10-11 20:57:49 +0300

    04 100/100 05 80/100

--- Java Fundamentals/Java Advanced/Excersises/FunctionalProgramming/04_FindEvensorOdds/src/FindEvensorOdds.java
+++ Java Fundamentals/Java Advanced/Excersises/FunctionalProgramming/04_FindEvensorOdds/src/FindEvensorOdds.java
@@ -1,6 +1,58 @@
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class FindEvensorOdds {
 
     public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        Function<String, String> fillArray = input -> {
+
+            Predicate<Integer> isEven = number -> number % 2 == 0;
+            Consumer<Integer> print = st -> { System.out.print(st + " "); };
+
+            int firstNumber = Integer.parseInt(input.split(" ")[0]);
+            int secondNumber = Integer.parseInt(input.split(" ")[1]);
+            String numberType = scanner.nextLine();
+            String numbers = "";
+
+            if (firstNumber > secondNumber) {
+
+                while (firstNumber >= secondNumber) {isOddEvenChecker(firstNumber, numberType, isEven, print);  firstNumber--;}
+
+
+            } else {
+
+                while (firstNumber <= secondNumber) {isOddEvenChecker(firstNumber, numberType, isEven, print);   firstNumber++;}
+
+            }
+
+            return numbers;
+
+        };
+
+        fillArray.apply(scanner.nextLine());
+
+    }
+
+    private static void isOddEvenChecker(int firstNumber, String oddEven, Predicate<Integer> isEven, Consumer<Integer> s) {
+
+        if (oddEven.equals("even")) {
+
+            if (isEven.test(firstNumber)) {
+                s.accept(firstNumber);
+            }
+
+        } else if (oddEven.equals("odd")){
+
+            if (!isEven.test(firstNumber)) {
+                s.accept(firstNumber);
+            }
+        }
 
     }
 }
--- Java Fundamentals/Java Advanced/Excersises/FunctionalProgramming/05_AppliedArithmetic/src/AppliedArithmetic.java
+++ Java Fundamentals/Java Advanced/Excersises/FunctionalProgramming/05_AppliedArithmetic/src/AppliedArithmetic.java
@@ -1,6 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
 public class AppliedArithmetic {
 
     public static void main(String[] args) {
 
+
+        Scanner scanner = new Scanner(System.in);
+
+        long[] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToLong(m -> Long.parseLong(m)).toArray();
+
+        Consumer<Long>  print = x -> System.out.print(x + " ");
+
+        String operations = scanner.nextLine();
+
+        while (!operations.equals("end")) {
+
+            for (int  i = 0; i < numbers.length; i++) {
+
+                switch (operations) {
+
+                    case "add": numbers[i]+=1;;break;
+                    case "multiply":numbers[i]*=2; break;
+                    case "subtract":numbers[i]-=1; break;
+                    case "print": print.accept(numbers[i]);break;
+                }
+            }
+
+            operations = scanner.nextLine();
+        }
     }
 }
