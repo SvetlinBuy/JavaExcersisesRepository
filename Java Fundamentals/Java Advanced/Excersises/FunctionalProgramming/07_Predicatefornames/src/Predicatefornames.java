eae36764d14fbcd870afc87ca5cebca2126ac297
Svetlin Buy <svetlinbuy@gmail.com>
2017-10-12 20:00:15 +0300

    07 100/100

--- Java Fundamentals/Java Advanced/Excersises/FunctionalProgramming/07_Predicatefornames/src/Predicatefornames.java
+++ Java Fundamentals/Java Advanced/Excersises/FunctionalProgramming/07_Predicatefornames/src/Predicatefornames.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+import java.util.function.Predicate;
+
 public class Predicatefornames {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        String[] names = scanner.nextLine().split(" ");
+
+        Predicate<String> isValidName = s -> s.length() <= n;
+
+        for (int i = 0; i < names.length; i++) {
+
+            if (isValidName.test(names[i])) {
+
+                System.out.println(names[i]);
+            }
+        }
 
     }
 }
