c6e7c564e821a73fee9a0f5702b6cb8c69d61888
Svetlin Buy <svetlinbuy@gmail.com>
2017-10-12 22:42:31 +0300

    09 100/100 10 100/100

--- Java Fundamentals/Java Advanced/Excersises/FunctionalProgramming/09_CustomComparator/src/CustomComparator.java
+++ Java Fundamentals/Java Advanced/Excersises/FunctionalProgramming/09_CustomComparator/src/CustomComparator.java
@@ -1,6 +1,47 @@
+import java.util.*;
+
 public class CustomComparator {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] nums = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(m -> Integer.parseInt(m)).toArray();
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            list.add(nums[i]);
+        }
+
+        Collections.sort(list, new EvenOddComparator());
+
+        for (int i = 0; i < list.size(); i++) {
+
+            System.out.print(list.get(i) + " ");
+        }
+
+    }
+
+    private static class EvenOddComparator implements Comparator<Integer>
+    {
+        final int BEFORE = -1;
+        final int EQUAL = 0;
+        final int AFTER = 1;
+
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            if (o1 % 2 == 0 && o2 % 2 != 0) {
+                return BEFORE;
+            } else if (o1 % 2 != 0 && o2 % 2 == 0) {
+                return AFTER;
+            } else if (o1 % 2 == 0 && o2 % 2 == 0) {
+                return o1.compareTo(o2);
+            } else if (o1 % 2 != 0 && o2 % 2 != 0) {
+                return o1.compareTo(o2);
+            }
+            return EQUAL;
+        }
 
     }
 }
--- Java Fundamentals/Java Advanced/Excersises/FunctionalProgramming/10_ListofPredicates/src/ListofPredicates.java
+++ Java Fundamentals/Java Advanced/Excersises/FunctionalProgramming/10_ListofPredicates/src/ListofPredicates.java
@@ -1,6 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
 public class ListofPredicates {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int N = Integer.parseInt(scanner.nextLine());
+
+        int[] divList = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(m -> Integer.parseInt(m)).toArray();
+
+
+        for (int i = 1; i <= N; i++) {
+
+            boolean test = true;
+
+            for (int j = 0; j < divList.length; j++) {
+
+                int n = divList[j];
+
+                Predicate<Integer> isDivisible = d -> d % n == 0;
+
+                if (!isDivisible.test(i)) {
+
+                    test = false;
+                }
+
+            }
+
+            if (test) {
+                System.out.print(i + " ");
+            }
+        }
 
     }
 }
