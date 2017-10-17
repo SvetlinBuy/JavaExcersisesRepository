206d15ae66b6f194b8a8c27d83ade7906375bec3
Svetlin Buy <svetlinbuy@gmail.com>
2017-10-17 05:22:10 +0300

    adding 10 87/100

--- Java Fundamentals/Java Advanced/Excersises/StreamAPI/10_LittleJohn/src/LittleJohn.java
+++ Java Fundamentals/Java Advanced/Excersises/StreamAPI/10_LittleJohn/src/LittleJohn.java
@@ -1,10 +1,57 @@
-
+import java.util.Scanner;
+import java.util.StringJoiner;
 
 public class LittleJohn {
 
     public static void main(String[] args) {
 
 
+        Scanner scanner = new Scanner(System.in);
+        int[] count = {0,0,0};
+        String[] arrows = {">----->", ">>----->", ">>>----->>"};
+
+        for (int i = 0; i < 4; i++) {
+
+            String row = scanner.nextLine();
+
+            while(row.contains(arrows[2])){
+
+                count[2]++;
+
+                row = row.replaceFirst(arrows[2], "");
+            }
+
+
+            while(row.contains(arrows[1])) {
+
+                count[1]++;
+
+                row = row.replaceFirst(arrows[1], "");
+            }
+
+           while(row.contains(arrows[0])) {
+
+                count[0]++;
+               row = row.replaceFirst(arrows[0], "");
+            }
+        }
+
+
+        int toConvert = Integer.parseInt(count[0] +""+ count[1] +""+ count[2] + "");
+
+        String binaryString = Integer.toBinaryString(toConvert);
+
+        StringBuilder sb = new StringBuilder("");
+
+        sb.append(binaryString);
+
+        sb.reverse();
+
+        String reversed = sb.toString();
+
+        int convertedNumber =  Integer.parseInt(binaryString + reversed, 2);
+
+        System.out.println(convertedNumber);
 
     }
 
